a1e0d534c6ac262c28656f4a63c165c42fefbd2b
Gabriel Huettenberger <gabriel@huettenberger.de>
2019-07-02 20:28:19 +1000

    fixed too rare refreshs

--- modules/tracker/me/src/moving-edges/vpMeEllipse.cpp
+++ modules/tracker/me/src/moving-edges/vpMeEllipse.cpp
@@ -233,7 +233,7 @@ void vpMeEllipse::reSample(const vpImage<unsigned char> &I)
 
   unsigned int n = numberOfSignal();
   expecteddensity = (alpha2 - alpha1) / vpMath::rad((double)me->getSampleStep());
-  if ((double)n < 0.5 * expecteddensity) {
+  if ((double)n < 0.9 * expecteddensity) {
     sample(I);
     vpMeTracker::track(I);
   }
